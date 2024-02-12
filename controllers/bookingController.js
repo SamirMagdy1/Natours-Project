57f5fdf3738125f22cc9d8f50ca2adda0fed868f
Samir Magdy <>
2024-02-12 22:09:45 +0200

    try to fix adding new bookings in db bug t1 modified:   controllers/bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,10 +60,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].unit_amount / 100;
-  await Booking.create({ tour, user, price });
+  try {
+    const tour = session.client_reference_id;
+    const user = (await User.findOne({ email: session.customer_email })).id;
+    const price = session.data.object.amount_total / 100;
+    await Booking.create({ tour, user, price });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 exports.webhookCheckout = (req, res, next) => {
