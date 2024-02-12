af82df8324b62dce31bd5138e079c9f1c69e4a34
Samir Magdy <>
2024-02-12 12:56:02 +0200

    Implemented CORS vid 14.9

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +30,17 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Global Middlewares
+// Implement CORS
+app.use(cors());
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com',
+// }));
+
+// for allowing to complex requests [put, patch, delete] 'proflight phase
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+// to allow sending complex requests to a specific route form cross origin resourc shairing
 
 // serving static files
 app.use(express.static(path.join(__dirname, 'public')));
